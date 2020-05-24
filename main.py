2627fc5ea4f93912798e047b182fd83b67500810
jonmartz <jonmartz91@gmail.com>
2020-05-24 20:31:50 +0300

    generated midi vectors

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-# import librosa.display
 import spacy
-# import pretty_midi
+import pretty_midi
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras import layers
+import matplotlib.pyplot as plt
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
@@ -17,6 +16,57 @@ from keras.layers import Dense, Input, GlobalMaxPooling1D
 from keras.layers import Conv1D, MaxPooling1D, Embedding
 from keras.models import Model
 from keras.initializers import Constant
+
+
+def get_midis_and_texts(instances, dataset_dir, instrument_indexes):
+    midis = []
+    texts = []
+    failed_num = 0
+    for index, row in instances.iterrows():
+        artist = row[0].strip().replace(' ', '_')
+        song_name = row[1].strip().replace(' ', '_')
+        filename = '%s_-_%s.mid' % (artist, song_name)
+        failed_text = '\t'
+        try:
+            midi = pretty_midi.PrettyMIDI('%s/midi_files/%s' % (dataset_dir, filename))
+            for instrument in midi.instruments:
+                if not instrument.is_drum:
+                    instrument_id = instrument.program
+                    if instrument_id not in instrument_indexes:
+                        instrument_indexes[instrument_id] = len(instrument_indexes)
+            midis.append(midi)
+            texts.append(row[2])
+        except:
+            failed_text = ' FAILED'
+            failed_num += 1
+        print('%d/%d%s\t%s' % (index + 1, len(instances), failed_text, filename))
+    print('FAILED = %d/%d' % (failed_num, len(instances)))
+    return midis, texts
+
+
+def get_midi_vectors(midis, texts, instrument_indexes, mode):
+    midi_vectors = []
+    for midi, text in zip(midis, texts):
+        general_tempo = midi.estimate_tempo()
+        general_chroma = midi.get_chroma()
+        total_notes = sum(len(instrument.notes) for instrument in midi.instruments)
+        instruments_presence = [0] * len(instrument_indexes)  # one entry per instrument
+        total_velocity = sum(sum(general_chroma))
+        general_semitones = [sum(semitone) / total_velocity for semitone in general_chroma]
+        instruments_semitones = [0] * (len(instrument_indexes) * 12)  # one entry per instrument per semitone
+        for instrument in midi.instruments:
+            if not instrument.is_drum:
+                instrument_index = instrument_indexes[instrument.program]  # instrument's index in the midi vector
+                instrument_presence = len(instrument.notes) / total_notes
+                instruments_presence[instrument_index] = instrument_presence
+                instrument_chroma = instrument.get_chroma()
+                total_velocity = sum(sum(instrument_chroma))
+                instrument_semitones = [sum(semitone) / total_velocity for semitone in instrument_chroma]
+                start_index = 12 * instrument_index  # jump to the instrument's semitones section in the midi vector
+                instruments_semitones[start_index: start_index + 12] = instrument_semitones
+        midi_vector = [general_tempo] + instruments_presence + general_semitones + instruments_semitones
+        midi_vectors.append(midi_vector)
+    return midi_vectors
 
 
 def get_vocabulary_and_word_embeddings(texts_train, texts_test, nlp):
@@ -39,29 +89,29 @@ def get_train_and_test_sets(texts_train, texts_test, word_indexes, nlp):
     x_train, y_train, x_test, y_test = [], [], [], []
     for x, y, texts in [[x_train, y_train, texts_train], [x_test, y_test, texts_test]]:
         for text in texts:
-            text_as_indexes = [word_indexes[token.text] for token in nlp(text.strip())]
-            x.append(text_as_indexes[:-1])
-            y.append(text_as_indexes[1:])
+            text_word_indexes = [word_indexes[token.text] for token in nlp(text.strip())]
+            x.append(text_word_indexes[:-1])
+            y.append(text_word_indexes[1:])
     return x_train, y_train, x_test, y_test
 
 
-def get_data_train(x_train, y_train):
-    x_tensors = tuple((tf.constant(i) for i in x_train))
-    y_tensors = tuple((tf.constant(i) for i in y_train))
-    return tf.data.Dataset.from_tensors((x_tensors, y_tensors))
+def get_train_and_val_data(x_train, y_train, val_split):
+    val_len = int(len(x_train) * val_split)
+    x_train_tensors = tuple((tf.constant(i) for i in x_train[:-val_len]))
+    y_train_tensors = tuple((tf.constant(i) for i in y_train[:-val_len]))
+    x_val_tensors = tuple((tf.constant(i) for i in x_train[-val_len:]))
+    y_val_tensors = tuple((tf.constant(i) for i in y_train[-val_len:]))
+    train_data = tf.data.Dataset.from_tensors((x_train_tensors, y_train_tensors))
+    val_data = tf.data.Dataset.from_tensors((x_val_tensors, y_val_tensors))
+    return train_data, val_data
 
 
 def build_model(vocabulary_size, word_embeddings, layer_sizes):
     model = Sequential(name='RNN')
-    model.add(layers.Embedding(vocabulary_size,
-                               word_embeddings.shape[1],
-                               embeddings_initializer=Constant(word_embeddings),
-                               trainable=False))
+    model.add(layers.Embedding(vocabulary_size, word_embeddings.shape[1], trainable=False,
+                               embeddings_initializer=Constant(word_embeddings)))
     for layer_size in layer_sizes:
-        model.add(layers.LSTM(layer_size,
-                              return_sequences=True,
-                              # stateful=True,
-                              ))
+        model.add(layers.LSTM(layer_size, return_sequences=True))
     model.add(layers.Dense(vocabulary_size))
     model.compile(optimizer='adam', loss=loss)
     return model
@@ -72,48 +122,37 @@ def loss(labels, logits):
 
 
 def get_checkpoints_callback(checkpoint_dir):
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
-    return tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix, save_weights_only=True)
+    filepath_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+    return tf.keras.callbacks.ModelCheckpoint(filepath=filepath_prefix, monitor='val_loss', save_weights_only=True)
 
 
-def generate_text(model, start_string, word_indexes, vocabulary, text_len=30):
-    # Evaluation step (generating text using the learned model)
+def generate_text(model, input_word, word_indexes, vocabulary, text_len, temperature=1.0):
+    """
 
-    # Converting our start string to numbers (vectorizing)
-    # input_eval = [char2idx[s] for s in start_string]
-    input_eval = tf.expand_dims([word_indexes[start_string]], 0)
-
-    # Empty string to store our results
+    :param model:
+    :param input_word:
+    :param word_indexes:
+    :param vocabulary:
+    :param text_len:
+    :param temperature: low values result -> predictable text, high values -> surprising text.
+    :return:
+    """
+    current_word_index = tf.expand_dims([word_indexes[input_word]], 0)
     text_generated = []
-
-    # Low temperatures results in more predictable text.
-    # Higher temperatures results in more surprising text.
-    # Experiment to find the best setting.
-    temperature = 1.0
-
-    # Here batch size == 1
     model.reset_states()
     for i in range(text_len):
-        predictions = model(input_eval)
-        # remove the batch dimension
-        predictions = tf.squeeze(predictions, 0)
-
-        # using a categorical distribution to predict the character returned by the model
-        predictions = predictions / temperature
-        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
-
-        # We pass the predicted character as the next input to the model
-        # along with the previous hidden state
-        input_eval = tf.expand_dims([predicted_id], 0)
-
+        predicted_words = tf.squeeze(model(current_word_index), 0) / temperature  # remove the batch dimension
+        # use a categorical distribution to predict the next word
+        predicted_id = tf.random.categorical(predicted_words, num_samples=1)[-1, 0].numpy()
         text_generated.append(vocabulary[predicted_id])
+        current_word_index = tf.expand_dims([predicted_id], 0)
 
-    return (start_string + ' ' + ' '.join(text_generated))
+    return '%s %s' % (input_word, ' '.join(text_generated))
 
 
 def write_to_file(history, generated_texts, layer_sizes, iteration):
     layers_str = '_'.join([str(i) for i in layer_sizes])
-    dir_name = 'drive/My Drive/Colab results/lyric generator results/layers_%s/%d' % (layers_str, iteration)
+    dir_name = 'drive/My Drive/Colab results/lyric generator results/layers_%s' % layers_str
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
     to_file = []
@@ -136,46 +175,34 @@ def write_to_file(history, generated_texts, layer_sizes, iteration):
         for line in to_file:
             file.write("%s\n" % line)
 
-    with open('%s/test_results.csv' % dir_name, 'w') as file:
+    with open('%s/test_results_%d.csv' % (dir_name, iteration), 'w') as file:
         for line in generated_texts:
             file.write("%s\n" % line)
 
 
-# def plot_piano_roll(pm, start_pitch, end_pitch, fs=100):
-#     # Use librosa's specshow function for displaying the piano roll
-#     librosa.display.specshow(pm.get_piano_roll(fs)[start_pitch:end_pitch],
-#                              hop_length=1, sr=fs, x_axis='time', y_axis='cqt_note',
-#                              fmin=pretty_midi.note_number_to_hz(start_pitch))
-#
-#
-# def pretty_midi_test():
-#     pm = pretty_midi.PrettyMIDI('dataset/midi_files/2_Unlimited_-_Get_Ready_for_This.mid')
-#
-#     # plt.figure(figsize=(12, 4))
-#     # plot_piano_roll(pm, 24, 84)
-#     # plt.show()
-#
-#     # Let's look at what's in this MIDI file
-#     print('There are {} time signature changes'.format(len(pm.time_signature_changes)))
-#     print('There are {} instruments'.format(len(pm.instruments)))
-#     print('Instrument 3 has {} notes'.format(len(pm.instruments[0].notes)))
-#     print('Instrument 4 has {} pitch bends'.format(len(pm.instruments[4].pitch_bends)))
-#     print('Instrument 5 has {} control changes'.format(len(pm.instruments[5].control_changes)))
-
-
-# dataset_dir = 'dataset'  # for pc
-dataset_dir = 'DL3/dataset'  # for colab
-texts_train = pd.read_csv('%s/lyrics_train_set.csv' % dataset_dir, header=None)[2]
-texts_test = pd.read_csv('%s/lyrics_test_set.csv' % dataset_dir, header=None)[2]
+mode = 'complete song'
+# mode = 'sliced song'
+dataset_dir = 'dataset'  # for pc
+# dataset_dir = 'DL3/dataset'  # for colab
+instances_train = pd.read_csv('%s/lyrics_train_set.csv' % dataset_dir, header=None)
+instances_test = pd.read_csv('%s/lyrics_test_set.csv' % dataset_dir, header=None)
+instrument_indexes = {}
+print('loading midis and texts...')
+midis_train, texts_train = get_midis_and_texts(instances_train, dataset_dir, instrument_indexes)
+midis_test, texts_test = get_midis_and_texts(instances_test, dataset_dir, instrument_indexes)
+print('generating midi vectors...')
+midi_vectors_train = get_midi_vectors(midis_train, texts_train, instrument_indexes, mode)
+midi_vectors_test = get_midi_vectors(midis_test, midis_test, instrument_indexes, mode)
 
 print('building vocabulary...')
-nlp = spacy.load('en_core_web_md')  # 300 dim embeddings
-# nlp = spacy.load('en_core_web_sm')  # smaller embeddings
+# nlp = spacy.load('en_core_web_md')  # 300 dim embeddings
+nlp = spacy.load('en_core_web_sm')  # smaller embeddings
 word_indexes, word_embeddings, vocabulary = get_vocabulary_and_word_embeddings(texts_train, texts_test, nlp)
 
 print('building train and test sets...')
+val_split = 0.2
 x_train, y_train, x_test, y_test = get_train_and_test_sets(texts_train, texts_test, word_indexes, nlp)
-data_train = get_data_train(x_train, y_train)
+train_data, val_data = get_train_and_val_data(x_train, y_train, val_split)
 
 # build model
 layer_sizes = [128]
@@ -185,7 +212,7 @@ checkpoint_dir = './training_checkpoints'
 checkpoint_callback = get_checkpoints_callback(checkpoint_dir)
 
 print('training model...')
-history = model.fit(data_train, epochs=100, callbacks=[checkpoint_callback])
+history = model.fit(train_data, epochs=10, validation_data=val_data, callbacks=[checkpoint_callback])
 
 # get best checkpoint
 tf.train.latest_checkpoint(checkpoint_dir)
--- midi_processing.py
+++ midi_processing.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
 import pretty_midi
 import mir_eval.display
 import librosa.display
 import matplotlib.pyplot as plt
+from mido.midifiles.meta import KeySignatureError
 
 
 def plot_piano_roll(pm, start_pitch, end_pitch, fs=100):
@@ -21,15 +23,15 @@ def pretty_midi_test():
     plt.show()
 
     # Let's look at what's in this MIDI file
-    print('There are {} time signature changes'.format(len(pm.time_signature_changes)))  # todo: use this feature
+    print('There are {} time signature changes'.format(len(pm.time_signature_changes)))
     print('There are {} instruments'.format(len(pm.instruments)))
     print('Instrument 3 has {} notes'.format(len(pm.instruments[0].notes)))  # todo: use this feature, weighted presence of each instrument
     print('Instrument 4 has {} pitch bends'.format(len(pm.instruments[4].pitch_bends)))
     print('Instrument 5 has {} control changes'.format(len(pm.instruments[5].control_changes)))
 
     # plot tempo changes
-    times, tempo_changes = pm.get_tempo_changes()
-    plt.plot(times, tempo_changes, '.')
+    tempo_change_times, tempos = pm.get_tempo_changes()  # todo: use this to get tempo by time
+    plt.plot(tempo_change_times, tempos, '.')
     plt.xlabel('Time')
     plt.ylabel('Tempo')
     plt.show()
@@ -48,8 +50,13 @@ def pretty_midi_test():
     plt.xlim(25, 45)
     plt.show()
 
+    # todo: use this feature, can slice chroma to get song subset and also do per instrument
+    # a proxy for key
+    total_velocity = sum(sum(pm.get_chroma()))
+    print([sum(semitone) / total_velocity for semitone in pm.get_chroma()])
+
     # Plot a pitch class distribution - sort of a proxy for key
-    plt.bar(np.arange(12), pm.get_pitch_class_histogram())  # todo: use this feature
+    plt.bar(np.arange(12), pm.get_pitch_class_histogram())
     plt.xticks(np.arange(12), ['C', '', 'D', '', 'E', 'F', '', 'G', '', 'A', '', 'B'])
     plt.xlabel('Note')
     plt.ylabel('Proportion')
@@ -73,11 +80,78 @@ def pretty_midi_test():
     print([sum(semitone) / total_velocity for semitone in chroma])
 
 
-pretty_midi_test()
+def get_midis_and_texts(instances, dataset_dir, instrument_indexes):
+    midis = []
+    texts = []
+    failed_num = 0
+    for index, row in instances.iterrows():
+        artist = row[0].strip().replace(' ', '_')
+        song_name = row[1].strip().replace(' ', '_')
+        filename = '%s_-_%s.mid' % (artist, song_name)
+        failed_text = '\t'
+        try:
+            midi = pretty_midi.PrettyMIDI('%s/midi_files/%s' % (dataset_dir, filename))
+            for instrument in midi.instruments:
+                if not instrument.is_drum:
+                    instrument_id = instrument.program
+                    if instrument_id not in instrument_indexes:
+                        instrument_indexes[instrument_id] = len(instrument_indexes)
+            midis.append(midi)
+            texts.append(row[2])
+        except:
+            failed_text = ' FAILED'
+            failed_num += 1
+        print('%d/%d%s\t%s' % (index + 1, len(instances), failed_text, filename))
+    print('FAILED = %d/%d' % (failed_num, len(instances)))
+    return midis, texts
 
-# todo: features to use, beside the ones above
+
+def get_midi_vectors(midis, texts, instrument_indexes, mode):
+    midi_vectors = []
+    for midi, text in zip(midis, texts):
+        general_tempo = midi.estimate_tempo()
+        general_chroma = midi.get_chroma()
+        total_notes = sum(len(instrument.notes) for instrument in midi.instruments)
+        instruments_presence = [0] * len(instrument_indexes)  # one entry per instrument
+        total_velocity = sum(sum(general_chroma))
+        general_semitones = [sum(semitone) / total_velocity for semitone in general_chroma]
+        instruments_semitones = [0] * (len(instrument_indexes) * 12)  # one entry per instrument per semitone
+        for instrument in midi.instruments:
+            if not instrument.is_drum:
+                instrument_index = instrument_indexes[instrument.program]  # instrument's index in the midi vector
+                instrument_presence = len(instrument.notes) / total_notes
+                instruments_presence[instrument_index] = instrument_presence
+                instrument_chroma = instrument.get_chroma()
+                total_velocity = sum(sum(instrument_chroma))
+                instrument_semitones = [sum(semitone) / total_velocity for semitone in instrument_chroma]
+                start_index = 12 * instrument_index  # jump to the instrument's semitones section in the midi vector
+                instruments_semitones[start_index: start_index + 12] = instrument_semitones
+        midi_vector = [general_tempo] + instruments_presence + general_semitones + instruments_semitones
+        midi_vectors.append(midi_vector)
+    return midi_vectors
+
+
+# pretty_midi_test()
+
+# todo: features to use:
 """ 
-vector of instruments present
-for instruments: get_pitch_class_histogram(use_duration=False, use_velocity=False, normalize=False)
+tempo of song (pm.estimate_tempo())
+vector of instrument weighted presence (len(pm.instruments[0].notes) / sum(len(pm.instruments[i].notes) for i in pm.instruments))
+vector of average semi-key for each instrument (get_pitch_class_histogram(use_duration=True, use_velocity=True, normalize=True))
 """
+
+mode = 'complete song'
+# mode = 'sliced song'
+dataset_dir = 'dataset'  # for pc
+# dataset_dir = 'DL3/dataset'  # for colab
+instances_train = pd.read_csv('%s/lyrics_train_set.csv' % dataset_dir, header=None)
+instances_test = pd.read_csv('%s/lyrics_test_set.csv' % dataset_dir, header=None)
+instrument_indexes = {}
+print('loading midis and texts...')
+midis_train, texts_train = get_midis_and_texts(instances_train, dataset_dir, instrument_indexes)
+midis_test, texts_test = get_midis_and_texts(instances_test, dataset_dir, instrument_indexes)
+print('generating midi vectors...')
+midi_vectors_train = get_midi_vectors(midis_train, texts_train, instrument_indexes, mode)
+midi_vectors_test = get_midi_vectors(midis_test, midis_test, instrument_indexes, mode)
+print('done')
 
